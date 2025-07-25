fff218dc615f214d28b85512c2ca01ada7f1d4a5
Shreyash Rai <shreyashrai078@gmail.com>
2025-07-25 22:19:26 +0000

    Don't you think the greeting could take up more of the space lol. This i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,27 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-background text-foreground font-body selection:bg-primary/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col lg:flex-row justify-between items-start gap-8">
-            <div className="w-full lg:w-1/2 pr-4">
-              <WelcomeMessageWrapper />
-            </div>
-            <div className="w-full lg:w-auto">
-              <DateTimeDisplay />
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row gap-8 items-start">
-            {/* Left Column */}
-            <div className="w-full flex flex-col gap-8">
-              <TodoList />
-            </div>
-
-            {/* Right Column */}
-            <div className="w-full lg:w-auto flex flex-col items-end gap-8">
-              <div className="w-full lg:w-72">
-                <PomodoroTimer />
+        <div className="flex flex-col lg:flex-row gap-8">
+          {/* Left Column */}
+          <div className="w-full lg:w-2/3 flex flex-col gap-8">
+            <div className="flex justify-between items-start gap-8">
+              <div className="w-full">
+                <WelcomeMessageWrapper />
               </div>
+              <div className="w-full lg:w-auto hidden lg:block">
+                <DateTimeDisplay />
+              </div>
+            </div>
+            <TodoList />
+          </div>
+
+          {/* Right Column */}
+          <div className="w-full lg:w-1/3 flex flex-col gap-8">
+             <div className="w-full lg:w-auto lg:hidden">
+                <DateTimeDisplay />
+              </div>
+            <div className="w-full">
+              <PomodoroTimer />
             </div>
           </div>
         </div>
